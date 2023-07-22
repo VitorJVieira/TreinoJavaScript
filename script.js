39238ca32acdcecd29c5c1f999640cd6d224c97e
VitorJastrombek <vitorjastrombekvieira@gmail.com>
2023-07-22 13:17:12 -0300

    Criada página index.html para poder treinar condicionais em JavaScript

---
+++ script.js
@@ -0,0 +1,50 @@
+/* var jogador1 = 0;
+var jogador2 = 0;
+var placar;
+
+if (jogador1 > 0 && jogador2 == 0) {
+    console.log("Jogador 1 marcou ponto");
+    placar = jogador1 > jogador2;
+} else if (jogador2 > 0 && jogador1 == 0) {
+    console.log("jogador 2 marcou ponto");
+    placar = jogador2 > jogador1;
+} else {
+    console.log('ninguem marcou ponto');
+};
+
+switch (placar){
+    case placar = jogador1 > jogador2:
+        console.log("Jogador 1 ganhou");
+        break;
+    case placar = jogador2 > jogador1:
+        console.log("jogador 2 ganhou");
+        break;
+    default:
+        console.log("o jogo empatou")
+} */
+
+let array = ["valor1", "valor2", "valor3", "valor4", "valor5"];
+/*
+for (i =0; i <array.length; i++){
+    console.log(array[i]);
+}
+
+for (i in array){
+    console.log(array[i]);
+}
+
+for (i of array) {
+    console.log(i)
+}*/
+
+var a = 0;
+
+// while (a < 10){
+//     a++;
+//     console.log(a);
+// }
+
+do {
+    a++;
+    console.log(a);
+} while (a<10);
